86cc792090ca907bee9ce54c78f194a5548d2ce2
SuXueFeng <5392510@qq.com>
2019-03-20 21:50:27 +0800

    增加查询签到

---
+++ dindin_usersign/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "钉钉办公-用户签到记录",
+    'summary': """钉钉办公-用户签到记录""",
+    'description': """ 钉钉办公-用户签到记录 """,
+    'author': "SuXueFeng",
+    'website': "https://www.sxfblog.com",
+    'category': 'dindin',
+    'version': '1.0',
+    'depends': ['base', 'ali_dindin'],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'data/system_conf.xml',
+        'views/department_sign.xml',
+        'views/users_sign.xml',
+    ],
+    # 'qweb': [
+    #     'static/xml/*.xml'
+    # ]
+
+}
---
+++ dindin_usersign/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import department_sign
+from . import users_sign
