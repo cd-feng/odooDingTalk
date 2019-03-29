0502a67fd8e14e7e10628a0cdc9266df01b5a104
SuXueFeng <5392510@qq.com>
2019-03-29 16:48:27 +0800

    增加仪表盘的公告功能

--- dindin_dashboard/models/blackboard.py
+++ dindin_dashboard/models/blackboard.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
-from odoo import api, fields, models
+import requests
+from requests import ReadTimeout
+from odoo import api, models
+
 _logger = logging.getLogger(__name__)
 
 
@@ -14,4 +18,27 @@ class DinDinBlackboard(models.TransientModel):
         根据当前用户获取公告信息
         :return:
         """
-        logging.info(self.env.user.name)
+        logging.info(">>>开始获取用户{}的公告信息...".format(self.env.user.name))
+        uid = self.env.user.id
+        emp = self.env['hr.employee'].sudo().search([('user_id', '=', uid)])
+        if emp:
+            url = self.env['ali.dindin.system.conf'].search([('key', '=', 'get_user_blackboard')]).value
+            token = self.env['ali.dindin.system.conf'].search([('key', '=', 'token')]).value
+            data = {
+                'userid': emp[0].din_id,
+            }
+            headers = {'Content-Type': 'application/json'}
+            try:
+                result = requests.post(url="{}{}".format(url, token), headers=headers, data=json.dumps(data),
+                                       timeout=6)
+                result = json.loads(result.text)
+                if result.get('errcode') == 0:
+                    line_list = list()
+                    for line in result.get('blackboard_list'):
+                        line_list.append(line)
+                    return {'state': True, 'data': line_list, 'msg': ''}
+                else:
+                    return {'state': False, 'msg': '获取公告失败,详情为:{}'.format(result.get('errmsg'))}
+            except ReadTimeout:
+                return {'state': False, 'msg': '获取公告网络连接超时'}
+        logging.info(">>>获取用户公告信息结束...")
--- dindin_dashboard/static/src/js/dashboard.js
+++ dindin_dashboard/static/src/js/dashboard.js
@@ -1,19 +1,57 @@
 odoo.define('dindin.blackboard.info', function (require) {
     "use strict";
 
-    let AbstractAction = require('web.AbstractAction');
+    let Dialog = require('web.Dialog');
     let core = require('web.core');
+    let QWeb = core.qweb;
+    let rpc = require('web.rpc');
+    let AbstractAction = require('web.AbstractAction');
+
+
     let DinDinDashboard = AbstractAction.extend({
         template: 'DindinDashboardInfo',
+        setBlackboardData: function (data) {
+            let self = this;
+            self.$el.find('#blackboard_list').html(QWeb.render("DindinDashboardInfoLine", {
+                widget: self,
+                data: data,
+            }));
+        },
+        setBlackboardFalseData: function (data) {
+            let self = this;
+            self.$el.find('#blackboard_list').html(QWeb.render("DindinDashboardInfoLineFalse", {
+                widget: self,
+                data: data,
+            }));
+        },
+        setBlackboardDataNull: function () {
+            let self = this;
+            self.$el.find('#blackboard_list').html(QWeb.render("DindinDashboardInfoLineNull", {
+                widget: self,
+                data: [],
+            }));
+        },
+
         start: function () {
-            // alert('1212');
-        }
-
+            let self = this;
+            let def = rpc.query({
+                model: 'dindin.blackboard',
+                method: 'get_blackboard_by_user',
+                args: [],
+            }).then(function (result) {
+                if (result.state) {
+                    if (result.data.length == 0) {
+                        self.setBlackboardDataNull();
+                    } else {
+                        self.setBlackboardData(result.data);
+                    }
+                } else {
+                    self.setBlackboardFalseData(result);
+                }
+            });
+        },
     });
-
 
     core.action_registry.add('dindin_dashboard', DinDinDashboard);
     return DinDinDashboard;
-
-
 });
