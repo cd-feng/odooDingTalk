1d2dc999d8ccbe5e1eaea77bdb499a69bfaaa36c
SuXueFeng <5392510@qq.com>
2019-04-01 21:19:16 +0800

    完成用户增加的会回调反之值

--- dindin_callback/controllers/user.py
+++ dindin_callback/controllers/user.py
@@ -3,7 +3,6 @@ import time
 from odoo import http, _
 from odoo.addons.web.controllers.main import Home
 from odoo.exceptions import UserError
-import json
 from odoo.http import request
 from .dingtalk_crypto import DingTalkCrypto
 
@@ -12,6 +11,7 @@ _logger = logging.getLogger(__name__)
 
 class CallBack(Home, http.Controller):
 
+    # 通讯录用户增加
     @http.route('/callback/user_add_org', type='json', auth='public')
     def callback_user_add_org(self, **kw):
         json_str = request.jsonrequest
@@ -30,33 +30,33 @@ class CallBack(Home, http.Controller):
         nonce = request.httprequest.args['nonce']
         logging.info(">>>nonce: {}".format(nonce))
         # 解密
-        crypto = DingTalkCrypto(
-            call_back[0].aes_key,
-            call_back[0].token,
-            din_corpId
-        )
-        randstr, length, msg, suite_key = crypto.decrypt(json_str.get('encrypt'))
-        msg = json.loads(msg)
-        logging.info(">>>解密后的消息结果:{}".format(msg))
+        # crypto = DingTalkCrypto(
+        #     call_back[0].aes_key,
+        #     call_back[0].token,
+        #     din_corpId
+        # )
+        # randstr, length, msg, suite_key = crypto.decrypt(json_str.get('encrypt'))
+        # msg = json.loads(msg)
+        # logging.info(">>>解密后的消息结果:{}".format(msg))
         # 返回加密结果
-        return self.result(call_back[0].aes_key, call_back[0].token, din_corpId)
+        return self.result_success(call_back[0].aes_key, call_back[0].token, din_corpId)
 
-    def result(self, encode_aes_key, token, din_corpId):
+    def result_success(self, encode_aes_key, token, din_corpid):
         """
-        封装返回值
+        封装success返回值
         :param encode_aes_key:
         :param token:
-        :param din_corpId:
+        :param din_corpid:
         :return:
         """
         from .dingtalk.crypto import DingTalkCrypto as dtc
-        dingtalkCrypto = dtc(encode_aes_key, din_corpId)
+        dc = dtc(encode_aes_key, din_corpid)
         # 加密数据
-        encrypt = dingtalkCrypto.encrypt('success')
-        timestamp = str(int(round(time.time() * 1000)))
-        nonce = dingtalkCrypto.generateRandomKey(8)
+        encrypt = dc.encrypt('success')
+        timestamp = str(int(round(time.time())))
+        nonce = dc.generateRandomKey(8)
         # 生成签名
-        signature = dingtalkCrypto.generateSignature(nonce, timestamp, token, encrypt)
+        signature = dc.generateSignature(nonce, timestamp, token, encrypt)
         new_data = {
             'json': True,
             'data': {
