5a60710c2144bef4a2ee73daf4fc4e4cbb88e414
SuXueFeng <5392510@qq.com>
2019-04-04 13:53:48 +0800

    增加签到列表

--- dindin_callback/controllers/eventreceive.py
+++ dindin_callback/controllers/eventreceive.py
@@ -144,7 +144,7 @@ class CallBack(Home, http.Controller):
                     dobys = "审批开始-时间:{}".format(dn.strftime('%Y/%m/%d %H:%M:%S'))
                     oa_model.sudo().message_post(body=dobys, message_type='notification')
                 else:
-                    oa_model.sudo().write({'oa_state': '02', 'oa_url': msg.get('url')})
+                    oa_model.sudo().write({'oa_state': '02', 'oa_url': msg.get('url'), 'oa_message': "审批结束"})
                     dobys = "审批结束-时间:{}".format(dn.strftime('%Y/%m/%d %H:%M:%S'))
                     oa_model.sudo().message_post(body=dobys, message_type='notification')
         return True
