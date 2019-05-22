b8c7523f4c6c79f7fe51027a3c053c08a4a628a8
Feng <suxuefeng20@gmail.com>
2019-05-22 13:37:29 +0800

    加入了一个简单的错误提示

--- dindin_login/controllers/auto_login.py
+++ dindin_login/controllers/auto_login.py
@@ -20,7 +20,7 @@ class AutoLoginController(http.Controller):
         :param kw:
         :return:
         """
-        logging.info("检测到用户使用免登...")
+        logging.info(">>>用户正在使用免登...")
         data = {'corp_id': request.env['ir.config_parameter'].sudo().get_param('ali_dindin.din_corpId')}
         return request.render('dindin_login.dingding_auto_login', data)
 
@@ -32,28 +32,33 @@ class AutoLoginController(http.Controller):
         :return:
         """
         authCode = kw.get('authCode')
-        logging.info("临时授权码：{}".format(authCode))
         if authCode:
-            userid = self.get_user_info_by_auth_code(authCode)
-            logging.info(">>>获取的user_info为：{}".format(userid))
-            employee = request.env['hr.employee'].sudo().search([('din_id', '=', userid)])
-            if employee:
-                user = employee.user_id
-                if user:
-                    # 解密钉钉登录密码
-                    logging.info(u'>>>:解密钉钉登录密码')
-                    password = base64.b64decode(user.din_password)
-                    password = password.decode(encoding='utf-8', errors='strict')
-                    request.session.authenticate(request.session.db, user.login, password)
-                    return http.local_redirect('/web')
-                else:
-                    # 自动注册
-                    password = str(random.randint(100000, 999999))
-                    fail = request.env['res.users'].sudo().create_user_by_employee(employee.id, password)
-                    if not fail:
-                        return http.local_redirect('/dingding/auto/login/in')
+            get_result = self.get_user_info_by_auth_code(authCode)
+            if not get_result.get('state'):
+                return self._post_error_message(get_result.get('msg'))
+            userid = get_result.get('userid')
+            logging.info(">>>获取的user_id为：{}".format(userid))
+            if userid:
+                employee = request.env['hr.employee'].sudo().search([('din_id', '=', userid)])
+                if employee:
+                    user = employee.user_id
+                    if user:
+                        # 解密钉钉登录密码
+                        logging.info(u'>>>:解密钉钉登录密码')
+                        password = base64.b64decode(user.din_password)
+                        password = password.decode(encoding='utf-8', errors='strict')
+                        request.session.authenticate(request.session.db, user.login, password)
+                        return http.local_redirect('/web')
+                    else:
+                        # 自动注册
+                        password = str(random.randint(100000, 999999))
+                        fail = request.env['res.users'].sudo().create_user_by_employee(employee.id, password)
+                        if not fail:
+                            return http.local_redirect('/dingding/auto/login/in')
+                    return http.local_redirect('/web/login')
                 return http.local_redirect('/web/login')
-            return http.local_redirect('/web/login')
+        else:
+            return self._post_error_message("获取临时授权码失败,请检查钉钉开发者后台设置!")
 
     def get_user_info_by_auth_code(self, auth_code):
         """
@@ -67,10 +72,21 @@ class AutoLoginController(http.Controller):
         try:
             result = requests.get(url=url, timeout=5)
             result = json.loads(result.text)
-            logging.info(">>>钉钉登录获取用户信息返回结果{}".format(result))
             if result.get('errcode') != 0:
-                raise Exception('钉钉接口错误:' + result.get('errmsg') + ' | 接口地址:' + url)
+                return {'state': False, 'msg': "钉钉接口错误:{}".format(result.get('errmsg'))}
             else:
-                return result.get('userid')
+                return {'state': True, 'userid': result.get('userid')}
         except ReadTimeout:
-            raise Exception("免登超时！")
+            return {'state': False, 'msg': "免登超时,请重试!"}
+        except Exception as e:
+            return {'state': False, 'msg': "登录失败,异常信息:{}".format(str(e))}
+
+    def _post_error_message(self, message):
+        """
+        返回错误消息
+        :param message:
+        :return:
+        """
+        data = {'message': message}
+        return request.render('dindin_login.dingding_auto_login_message', data)
+
