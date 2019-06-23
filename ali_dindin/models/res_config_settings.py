19c0edc3849571aa2dabff5d853df9bf05f7d234
banqu <banqu@163.com>
2019-06-23 21:33:44 +0800

    在设置里添加模块安装选择界面 默认安装主模块后，除了仪表盘自动安装，其他模块由用户手动选择安装。

--- ali_dindin/models/res_config_settings.py
+++ ali_dindin/models/res_config_settings.py
@@ -24,6 +24,74 @@ class ResConfigSettings(models.TransientModel):
     din_login_appsecret = fields.Char(string=u'钉钉登录appSecret')
     auto_calendar_event = fields.Boolean(string=u'自动上传日程')
     
+    # 安装钉钉模块
+    module_dindin_attendance = fields.Boolean(
+        '钉钉办公-考勤排班',
+        help='钉钉办公-考勤排班.\n'
+        '- This installs the module dindin_attendance.'
+    )
+    module_dindin_calendar = fields.Boolean(
+        '钉钉办公-日程',
+        help='钉钉办公-日程.\n'
+        '- This installs the module dindin_calendar.'
+    )
+    module_dindin_approval = fields.Boolean(
+        '钉钉办公-审批',
+        help='钉钉办公-审批.\n'
+        '- This installs the module dindin_approval.'
+    )
+    module_dindin_callback = fields.Boolean(
+        '钉钉办公-回调管理',
+        help='钉钉办公-回调管理.\n'
+        '- This installs the module dindin_callback.'
+    )
+    module_dindin_dashboard = fields.Boolean(
+        '钉钉办公-仪表盘',
+        help='钉钉办公-仪表盘.\n'
+        '- This installs the module dindin_dashboard.'
+    )
+    module_dindin_login = fields.Boolean(
+        '钉钉办公-扫码与免登',
+        help='钉钉办公-扫码与免登.\n'
+        '- This installs the module dindin_login.'
+    )
+    module_dingding_hrm = fields.Boolean(
+        '钉钉办公-智能人事',
+        help='钉钉办公-智能人事.\n'
+        '- This installs the module dingding_hrm.'
+    )
+    module_dindin_message = fields.Boolean(
+        '钉钉办公-消息管理',
+        help='钉钉办公-消息管理.\n'
+        '- This installs the module dindin_message.'
+    )
+    module_dindin_message_template = fields.Boolean(
+        '钉钉办公-消息模板',
+        help='钉钉办公-消息模板.\n'
+        '- This installs the module dindin_message_template.'
+    )
+    module_dindin_report = fields.Boolean(
+        '钉钉办公-日志',
+        help='钉钉办公-日志.\n'
+        '- This installs the module dindin_report.'
+    )
+
+    module_dindin_usersign = fields.Boolean(
+        '钉钉办公-签到',
+        help='钉钉办公-签到.\n'
+        '- This installs the module dindin_usersign.'
+    )
+    module_dindin_workrecord = fields.Boolean(
+        '钉钉办公-待办事项',
+        help='钉钉办公-待办事项.\n'
+        '- This installs the module dindin_workrecord.'
+    )
+    module_dingding_health = fields.Boolean(
+        '钉钉办公-运动',
+        help='钉钉办公-运动.\n'
+        '- This installs the module dingding_health.'
+    )
+    
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
--- dindin_attendance/__manifest__.py
+++ dindin_attendance/__manifest__.py
@@ -10,7 +10,7 @@
     'depends': ['base', 'ali_dindin', 'mail', 'hr_attendance'],
     'installable': True,
     'application': False,
-    'auto_install': True,
+    'auto_install': False,
     'data': [
         'security/ir.model.access.csv',
         'security/dingding_security.xml',
