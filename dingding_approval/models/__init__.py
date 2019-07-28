53bc7f090452433cf67af4f5211d3553f9e7b9d5
Feng <suxuefeng20@gmail.com>
2019-07-28 10:48:21 +0800

    先清除不需要的模块

--- dingding_approval/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import approval_template
-from . import approval_control
-from . import main
--- dingding_usersign/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sign_list
-from . import department_sign
-from . import users_sign
