8fe63f3c37c6febab0da8baf5e431c0d09ce9554
Feng <suxuefeng20@gmail.com>
2019-09-01 13:38:20 +0800

    修复回调

--- dingding_base/controllers/callback_controller.py
+++ dingding_base/controllers/callback_controller.py
@@ -40,7 +40,6 @@ class DingDingCallBackManage(Home, http.Controller):
     # 钉钉回调
     @http.route('/dingding/callback/eventreceive', type='json', auth='none', methods=['POST'], csrf=False)
     def dingding_callback_controller(self, **kw):
-        logging.info(">>>dingding-eventreceive-start")
         json_str = request.jsonrequest
         callback, corp_id = self.get_bash_attr()
         msg = self.encrypt_result(json_str.get('encrypt'), callback.aes_key, corp_id)
@@ -49,27 +48,31 @@ class DingDingCallBackManage(Home, http.Controller):
         event_type = msg.get('EventType')
         # --------通讯录------
         if event_type == 'user_add_org' or event_type == 'user_modify_org' or event_type == 'user_leave_org':
-            return False
-            if event_type == 'user_leave_org':
-                UserId = msg.get('UserId')
-                for user_id in UserId:
+            if event_type == 'user_leave_org':   # 用户离职
+                user_ids = msg.get('UserId')
+                for user_id in user_ids:
                     emp = request.env['hr.employee'].sudo().search([('ding_id', '=', user_id)])
-                    emp.sudo().write({'active': False})
-            else:
-                request.env['dingding.bash.data.synchronous'].sudo().synchronous_dingding_employee()
-        # elif event_type == 'org_dept_create' or event_type == 'org_dept_modify' or event_type == 'org_dept_remove':
-        #     DeptId = msg.get('DeptId')
-        #     if event_type == 'org_dept_remove':
-        #         for dept in DeptId:
-        #             hr_depat = request.env['hr.department'].sudo().search([('ding_id', '=', dept)])
-        #             if hr_depat:
-        #                 hr_depat.sudo().unlink()
-        #     else:
-        #         request.env['dingding.bash.data.synchronous'].sudo().synchronous_dingding_department()
-        # # -----员工角色-------
-        # elif event_type == 'label_user_change' or event_type == 'label_conf_add' or event_type == 'label_conf_del' \
-        #         or event_type == 'label_conf_modify':
-        #     logging.info(">>>钉钉回调-员工角色信息发生变更/增加/删除/修改")
+                    if emp:
+                        emp.sudo().write({'active': False})
+            else:  # 用户增加和变更时获取该用户详情
+                user_ids = msg.get('UserId')
+                for user_id in user_ids:
+                    self.get_employee_info(user_id, event_type)
+        # --------部门------
+        elif event_type == 'org_dept_create' or event_type == 'org_dept_modify' or event_type == 'org_dept_remove':
+            dept_ids = msg.get('DeptId')
+            if event_type == 'org_dept_remove':
+                for dept_id in dept_ids:
+                    hr_depat = request.env['hr.department'].sudo().search([('ding_id', '=', dept_id)])
+                    if hr_depat:
+                        hr_depat.sudo().write({'active': False})
+            else:  # 部门增加和变更时获取该部门详情
+                for dept_id in dept_ids:
+                    self.get_department_info(dept_id, event_type)
+        # -----员工角色-------
+        elif event_type == 'label_user_change' or event_type == 'label_conf_add' or event_type == 'label_conf_del' \
+                or event_type == 'label_conf_modify':
+            logging.info(">>>钉钉回调-员工角色信息发生变更/增加/删除/修改")
         # -----审批-----------
         elif event_type == 'bpms_task_change':
             self.bpms_task_change(msg)
@@ -183,4 +186,87 @@ class DingDingCallBackManage(Home, http.Controller):
                     dobys = "{}: {}已审批; 审批结果:{}; 审批意见:{}".format(now_time, emp.name, OARESULT.get(msg.get('result')), msg.get('remark'))
                     oa_model.sudo().message_post(body=dobys, message_type='notification')
         return True
+
+    def get_employee_info(self, user_id, event_type):
+        """
+        从钉钉获取用户详情
+        :param user_id:
+        :param event_type:
+        :return:
+        """
+        url, token = request.env['dingding.parameter'].sudo().get_parameter_value_and_token('user_get')
+        data = {'userid': user_id}
+        result = request.env['dingding.api.tools'].send_get_request(url, token, data, 15)
+        if result.get('errcode') == 0:
+            data = {
+                'name': result.get('name'),  # 员工名称
+                'ding_id': result.get('userid'),  # 钉钉用户Id
+                'din_unionid': result.get('unionid'),  # 钉钉唯一标识
+                'mobile_phone': result.get('mobile'),  # 手机号
+                'work_phone': result.get('tel'),  # 分机号
+                'work_location': result.get('workPlace'),  # 办公地址
+                'notes': result.get('remark'),  # 备注
+                'job_title': result.get('position'),  # 职位
+                'work_email': result.get('email'),  # email
+                'din_jobnumber': result.get('jobnumber'),  # 工号
+                'din_avatar': result.get('avatar') if result.get('avatar') else '',  # 钉钉头像url
+                'din_isSenior': result.get('isSenior'),  # 高管模式
+                'din_isAdmin': result.get('isAdmin'),  # 是管理员
+                'din_isBoss': result.get('isBoss'),  # 是老板
+                'din_isHide': result.get('isHide'),  # 隐藏手机号
+                'din_active': result.get('active'),  # 是否激活
+                'din_isLeaderInDepts': result.get('isLeaderInDepts'),  # 是否为部门主管
+                'din_orderInDepts': result.get('orderInDepts'),  # 所在部门序位
+            }
+            # 支持显示国际手机号
+            if result.get('stateCode') != '86':
+                data.update({
+                    'mobile_phone': '+{}-{}'.format(result.get('stateCode'), result.get('mobile')),
+                })
+            if result.get('hiredDate'):
+                date_str = request.env['dingding.api.tools'].get_time_stamp(result.get('hiredDate'))
+                data.update({'din_hiredDate': date_str})
+            if result.get('department'):
+                dep_ding_ids = result.get('department')
+                dep_list = request.env['hr.department'].sudo().search([('ding_id', 'in', dep_ding_ids)])
+                data.update({'department_ids': [(6, 0, dep_list.ids)], 'department_id': dep_list[0].id if dep_list else False})
+            if event_type == 'user_add_org':
+                request.env['hr.employee'].sudo().create(data)
+            else:
+                employee = request.env['hr.employee'].sudo().search([('ding_id', '=', user_id)], limit=1)
+                if employee:
+                    employee.sudo().write(data)
+        else:
+            _logger.info("从钉钉同步员工时发生意外，原因为:{}".format(result.get('errmsg')))
+        return True
+
+    def get_department_info(self, dept_id, event_type):
+        """
+        获取部门详情
+        :param dept_id:
+        :param event_type:
+        :return:
+        """
+        url, token = request.env['dingding.parameter'].sudo().get_parameter_value_and_token('department_get')
+        data = {'id': dept_id}
+        result = request.env['dingding.api.tools'].send_get_request(url, token, data, 15)
+        if result.get('errcode') == 0:
+            data = {
+                'name': result.get('name'),
+                'ding_id': result.get('id'),
+            }
+            if result.get('parentid') != 1:
+                domain = [('ding_id', '=', result.get('parentid'))]
+                partner_department = request.env['hr.department'].sudo().search(domain, limit=1)
+                if partner_department:
+                    data.update({'parent_id': partner_department.id})
+            if event_type == 'org_dept_create':
+                request.env['hr.department'].sudo().create(data)
+            elif event_type == 'org_dept_modify':
+                domain = [('ding_id', '=', result.get('id')), ('active', '=', True)]
+                h_department = request.env['hr.department'].sudo().search(domain)
+                if h_department:
+                    h_department.sudo().write(data)
+        return True
+
 
