fcf77a8e2b5bf01096a92843a6810cbd5da76c14
Feng <suxuefeng20@gmail.com>
2019-10-15 13:41:59 +0800

    删除原集成模块

--- dingding_base/models/__init__.py
+++ dingding_base/models/__init__.py
@@ -28,4 +28,3 @@ from . import dingding_callback_list
 from . import dingding_callback_manage
 from . import dingding_approval_template
 from . import dingding_approval_control
-from . import dingding_approval_main
