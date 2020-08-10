2e35d09503833ac12093458f33a5032124bbcd00
粟雪峰 <>
2020-08-10 20:41:11 +0800

    delete

--- dingtalk_report/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#    Copyright (C) 2019 SuXueFeng GNU
-###################################################################################
-
-from . import dingtalk_report_template
-from . import dingtalk_report_list
--- web_progress/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import base
-from . import base_import
-from . import ir_actions_report
-from . import ir_cron
-from . import web_progress
