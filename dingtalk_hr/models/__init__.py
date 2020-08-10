15cbd12ed2e2cc63abe4f962f09762f6bd9ba909
粟雪峰 <>
2020-08-10 21:16:19 +0800

    delete old

--- dingtalk_hr/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#    Copyright (C) 2019 SuXueFeng GNU
-###################################################################################
-
-from . import hr_department
-from . import hr_employee
-from . import res_partner
--- dingtalk_report/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#    Copyright (C) 2019 SuXueFeng  GNU
-###################################################################################
-
-from . import report_category
-from . import report_report
-from . import report_template
