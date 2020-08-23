80d666782872266e5bc0f133f8706b5ca8188d26
粟雪峰 <>
2020-08-23 11:28:10 +0800

    暂存定义消息

---
+++ dingtalk_message/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "钉钉消息功能模块",
+    'summary': """自定义配置钉钉消息""",
+    'description': """消息通知""",
+    'author': "XueFeng.Su",
+    'website': "https://www.sxfblog.com",
+    'category': 'dingtalk',
+    'version': '1.0',
+    'depends': ['dingtalk_mc'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
+        'data/default_message_config.xml',
+        'views/dingtalk_msg_config.xml',
+    ],
+}
