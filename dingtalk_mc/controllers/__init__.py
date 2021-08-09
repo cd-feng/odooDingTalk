6f4a4d92a1da09d2b33cb8a518b31fb52be5e342
DESKTOP-7D211H1\suxuefeng <5392510@qq.com>
2021-08-09 10:46:37 +0800

    update

--- dingtalk_mc/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import ir_http
-from . import login_controller
-from . import callback_controller
-from . import crypto
--- dingtalk_mc/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import dingtalk_tool
-from . import odoo_model
-from . import ir_ui_view
-from . import dingtalk_approval
-from . import dingtalk_robot_api
